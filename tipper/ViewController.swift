d25c878998044cb65dc4f60bb69cabec58f5ded5
SandeshOnRails <basnetsandesh450@gmail.com>
2018-08-20 16:20:32 -0700

    basic project complete

--- tipper/ViewController.swift
+++ tipper/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipAmount: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        
+       view.endEditing(true)
+    }
+    
+    @IBAction func calculate(_ sender: Any) {
+        
+        
+        let  allTips = [0.18, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        
+        let tip = bill *  allTips[tipAmount.selectedSegmentIndex]
+        
+        
+        let total = bill + tip
+       
+        
+        tipLabel.text = String(format:
+        "$%.2f", tip )
+        totalLabel.text = String(format: "$%.2f",total )
+        
+    }
+    
+   
+    
+    
 }
 
